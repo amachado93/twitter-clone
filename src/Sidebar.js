75f5a54f2feaed39ddebc8aa5c0d1ec541a8c881
Alex Machado <amachado@sva.edu>
2021-04-24 19:55:22 -0400

    Add SidebarOptions and Icons

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,6 +1,17 @@
 import React from "react";
 import './Sidebar.css';
+import SidebarOption from './SidebarOption';
+
+//icon imports
 import TwitterIcon from '@material-ui/icons/Twitter';
+import HomeIcon from '@material-ui/icons/Home';
+import SearchIcon from '@material-ui/icons/Search';
+import NotificationsNoneIcon from '@material-ui/icons/NotificationsNone';
+import MailOutlineIcon from '@material-ui/icons/MailOutline';
+import BookmarkBorderIcon from '@material-ui/icons/BookmarkBorder';
+import ListAltIcon from '@material-ui/icons/ListAlt';
+import PermIdentityIcon from '@material-ui/icons/PermIdentity';
+import MoreHorizIcon from '@material-ui/icons/MoreHoriz';
 
 function Sidebar() {
     return(
@@ -8,13 +19,15 @@ function Sidebar() {
         {/*twitter icon*/}
         <TwitterIcon />
 
-
-        {/*SidebarOption*/}
-        {/*SidebarOption*/}
-        {/*SidebarOption*/}
-        {/*SidebarOption*/}
-        {/*SidebarOption*/}
-        {/*SidebarOption*/}
+        {/*SidebarOptions*/}
+        <SidebarOption Icon={HomeIcon} text="Home" />
+        <SidebarOption Icon={SearchIcon} text="Explore" />
+        <SidebarOption Icon={NotificationsNoneIcon} text="Notifications" />
+        <SidebarOption Icon={MailOutlineIcon} text="Messages" />
+        <SidebarOption Icon={BookmarkBorderIcon} text="Bookmarks" />
+        <SidebarOption Icon={ListAltIcon} text="Lists" />
+        <SidebarOption Icon={PermIdentityIcon} text="Profile" />
+        <SidebarOption Icon={MoreHorizIcon} text="More" />
 
         </div>
     );
