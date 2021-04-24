29983eb143fbbad9c4febab8d993b789c0963481
Alex Machado <amachado@sva.edu>
2021-04-24 18:56:06 -0400

    Start Sidebar.js and Sidebar.css

---
+++ src/Sidebar.js
@@ -0,0 +1,17 @@
+import React from "react";
+import './Sidebar.css';
+
+function Sidebar() {
+    return(
+        <div className="sidebar">
+        // twitter icon
+        // SidebarOption
+        // SidebarOption
+        // SidebarOption
+        // SidebarOption
+        // SidebarOption
+        // SidebarOption
+
+        </div>
+    );
+}
