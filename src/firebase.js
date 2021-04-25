486e500fe796d6711fc089955d796e997edb7c04
Alex Machado <amachado@sva.edu>
2021-04-24 23:34:15 -0400

    Initialize Firebase

---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDhERM5jQe3XNKnZ0Z7_WrJ_3ep3pNX58A",
+    authDomain: "twitter-clone-61ef4.firebaseapp.com",
+    projectId: "twitter-clone-61ef4",
+    storageBucket: "twitter-clone-61ef4.appspot.com",
+    messagingSenderId: "354145132216",
+    appId: "1:354145132216:web:88467443cca83fdb4fde4d",
+    measurementId: "G-279M406ENN"
+  };
